6b40c7c336b5a0cf1dc8d7e6b38f221007fa1508
Austin Dulay <dulayaustin@gmail.com>
2016-07-06 16:21:45 +0800

    Create cart, session management, rel of models

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,17 @@
 class StoreController < ApplicationController
+  before_filter :increment_index_access_counter
+  
   def index
-    @products = Product.order(:title)
+    @products = Product.order(:title)   
+  end
+
+  def increment_index_access_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+      @increment_counter =  session[:counter]
+    else
+      session[:counter] += 1
+      @increment_counter = session[:counter]
+    end
   end
 end
